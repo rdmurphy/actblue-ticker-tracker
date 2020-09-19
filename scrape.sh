8644673baafb71d5184f640d730098777d477d76
Ryan Murphy <rdmurphy@users.noreply.github.com>
2020-09-18 22:40:27 -0700

    Start it up

---
+++ scrape.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# our input/output file
+FILE=sitewide_raised_amount.txt
+
+PREVIOUS_AMOUNT=$(cat ${FILE})
+NEW_AMOUNT=$(curl https://secure.actblue.com/metrics/bignumber | jq --raw-output .sitewide_raised_amount | sed 's/,//g')
+
+echo -n "$NEW_AMOUNT" > "$FILE"
+
+DIFFERENCE=$((${NEW_AMOUNT} - ${PREVIOUS_AMOUNT}))
+
+echo "::set-output name=difference::${DIFFERENCE}"
