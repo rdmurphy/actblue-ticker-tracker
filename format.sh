fa0b905728acb88cc03aeaa3539bed39f0f988d9
Ryan Murphy <rdmurphy@users.noreply.github.com>
2021-02-06 12:03:45 -0800

    Come on

--- format.sh
+++ format.sh
@@ -2,7 +2,7 @@
 
 echo "amount,timestamp" > amounts.csv
 
-for commit in $(git log --full-history --pretty=format:%h sitewide_raised_amount.txt)
+for commit in $(git log --pretty=format:%h sitewide_raised_amount.txt)
 do
   echo "$(git show ${commit}:sitewide_raised_amount.txt),$(git show -s --format=%at ${commit})" >> amounts.csv
 done
