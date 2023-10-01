109c627d213e67b2b380a8d91dd0022afaa1916b
Ryan Murphy <rdmurphy@users.noreply.github.com>
2023-10-01 12:42:06 -0700

    Let's test out this idea

---
+++ insert.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# get the commit hash of the latest commit of sitewide_raised_amount.txt
+commit=$(git log -1 --pretty=format:%h sitewide_raised_amount.txt)
+
+# get the amount from the latest commit of sitewide_raised_amount.txt
+amount=$(git show ${commit}:sitewide_raised_amount.txt)
+
+# get the timestamp from the latest commit of sitewide_raised_amount.txt
+timestamp=$(git show -s --format=%at ${commit})
+
+# insert the amount and timestamp into amounts.csv on the second line
+sed -i "2i${amount},${timestamp}" tmp.csv
